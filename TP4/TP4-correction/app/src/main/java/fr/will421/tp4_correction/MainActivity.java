3d7fd2afc1333f9cb05c0c27776d75c78e9ee48d
WB <william.bobo@hardis.fr>
2018-03-22 11:09:59 +0100

    Afficher une notification lors de l'appui sur le bouton

--- TP4/TP4-correction/app/src/main/java/fr/will421/tp4_correction/MainActivity.java
+++ TP4/TP4-correction/app/src/main/java/fr/will421/tp4_correction/MainActivity.java
@@ -1,13 +1,34 @@
 package fr.will421.tp4_correction;
 
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private static int NOTIFICATION_ID=  1;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+        final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle("Hello world")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    
+    
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+            }
+        });
     }
 }
