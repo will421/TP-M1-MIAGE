21e4fde18d55a41fb10cffec5c8c67ee3cfbe78c
WB <william.bobo@hardis.fr>
2018-03-22 10:11:37 +0100

    Init project

---
+++ TP4/TP4-correction/app/src/main/java/fr/will421/tp4_correction/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.will421.tp4_correction;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
